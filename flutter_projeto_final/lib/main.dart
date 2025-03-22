02835c229d18ebf524d95c969dbd77192a109293
andersonaires-cc <andersonairesspp@hotmail.com>
2025-03-22 20:36:55 -0300

    feat(main): Fazendo o projeto rodar local

--- flutter_projeto_final/lib/main.dart
+++ flutter_projeto_final/lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(.
+      theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page update '),
     );
   }
 }
