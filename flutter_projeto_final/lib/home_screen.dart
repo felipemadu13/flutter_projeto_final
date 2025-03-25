a68db87725978ff86e39a3895e7e0881d91b33fb
felipemadu13 <felipemadureira.a@hotmail.com>
2025-03-24 21:01:59 -0300

    feat: noticias com imagens

--- flutter_projeto_final/lib/home_screen.dart
+++ flutter_projeto_final/lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,42 +10,84 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> noticias = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNoticias();
+  }
+
+  Future<void> fetchNoticias() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/noticias'));
+    if (response.statusCode == 200) {
+      setState(() {
+        noticias = json.decode(utf8.decode(response.bodyBytes));
+      });
+    } else {
+      throw Exception('Falha ao carregar as notícias');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('Texto Centralizado'),
-            ],
+        child: noticias.isEmpty
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: noticias.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(noticias[index]['imagemUrl']),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            noticias[index]['titulo'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(noticias[index]['texto']),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.newspaper),
+            label: 'Notícias',
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed, 
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.newspaper),
-              label: 'Notícias',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.post_add),
-              label: 'Criar',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.view_agenda),
-              label: 'Agendamentos',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Perfil'
-            ),
-          ],
-          selectedItemColor: Color.fromARGB(255, 53, 138, 118),
-          onTap: (int index) {
-            print('Item $index');
-          },
+          BottomNavigationBarItem(
+            icon: Icon(Icons.post_add),
+            label: 'Criar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.view_agenda),
+            label: 'Agendamentos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        selectedItemColor: Color.fromARGB(255, 53, 138, 118),
+        onTap: (int index) {
+          print('Item $index');
+        },
       ),
     );
   }
