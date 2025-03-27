c5f301aa07e71ec4f0c837f920f6ce09f96156b4
felipemadu13 <felipemadureira.a@hotmail.com>
2025-03-27 07:53:00 -0300

    feat: barra para pesquisa de noticias

--- flutter_projeto_final/lib/ui/page/home_screen.dart
+++ flutter_projeto_final/lib/ui/page/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_projeto_final/ui/widgets/bottom_nav.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -13,7 +12,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<dynamic> noticias = [];
+  List<dynamic> filteredNoticias = [];
   int _selectedIndex = 0;
+  String searchQuery = '';
 
   @override
   void initState() {
@@ -26,10 +27,21 @@ class _HomeScreenState extends State<HomeScreen> {
     if (response.statusCode == 200) {
       setState(() {
         noticias = json.decode(utf8.decode(response.bodyBytes));
+        filteredNoticias = noticias; // Inicialmente, todas as notícias são exibidas
       });
     } else {
       throw Exception('Falha ao carregar as notícias');
     }
+  }
+
+  void _onSearch(String query) {
+    setState(() {
+      searchQuery = query.toLowerCase();
+      filteredNoticias = noticias
+          .where((noticia) =>
+              noticia['titulo'].toLowerCase().contains(searchQuery))
+          .toList();
+    });
   }
 
   void _onItemTapped(int index) {
@@ -44,26 +56,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          onChanged: _onSearch,
+          decoration: InputDecoration(
+            hintText: 'Pesquisar notícias...',
+            border: InputBorder.none,
+            hintStyle: TextStyle(color: Colors.white70),
+          ),
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color.fromARGB(255, 41, 109, 94),
+      ),
       body: Center(
-        child: noticias.isEmpty
+        child: filteredNoticias.isEmpty
             ? CircularProgressIndicator()
             : ListView.builder(
-                itemCount: noticias.length,
+                itemCount: filteredNoticias.length,
                 itemBuilder: (context, index) {
                   return Card(
                     margin: EdgeInsets.all(20),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Image.network(noticias[index]['imagemUrl']),
+                        Image.network(filteredNoticias[index]['imagemUrl']),
                         Padding(
                           padding: const EdgeInsets.all(10),
                           child: Text(
-                            noticias[index]['titulo'],
+                            filteredNoticias[index]['titulo'],
                             style: TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 41, 109, 94)
+                              color: Color.fromARGB(255, 41, 109, 94),
                             ),
                           ),
                         ),
